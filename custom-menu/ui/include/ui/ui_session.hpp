74e45204e47dc94c9fb573243cd3e49cf103389e
Davide Depau <davide@depau.eu>
2025-12-26 00:25:48 +0100

    fix: avoid linking ui to the custom menu

--- custom-menu/ui/include/ui/ui_session.hpp
+++ custom-menu/ui/include/ui/ui_session.hpp
@@ -28,12 +28,24 @@ public:
    *
    * @param button The button that was pressed
    */
-  void handle_keypress(int button) const;
+  void handle_keypress(int button) const {
+    assert(controller_api != nullptr && "Controller API is null");
+    if (!screen_stack.empty()) {
+      screen_stack.back()->handle_keypress(*controller_api, button);
+    }
+  }
 
   /**
    * Render the top screen in the stack.
    */
-  void render() const;
+  void render() const {
+    assert(controller_api != nullptr && "Controller API is null");
+    if (!screen_stack.empty()) {
+      screen_stack.back()->render(*controller_api);
+    } else {
+      controller_api->goto_main_menu();
+    }
+  }
 
   /**
    * Push a new screen onto the stack and render it.
@@ -65,20 +77,35 @@ public:
    * Pop the top screen from the stack and render the new top screen.
    * If the stack becomes empty, return to the main menu.
    */
-  void pop_screen();
+  void pop_screen() {
+    assert(controller_api != nullptr && "Controller API is null");
+    if (!screen_stack.empty()) {
+      screen_stack.pop_back();
+    }
+  }
 
   /**
    * Get the number of ticks per second needed by the top screen in the stack.
    *
    * @return The number of ticks per second needed. Returns 0 if the stack is empty.
    */
-  [[nodiscard]] int needs_ticks_per_second() const;
+  [[nodiscard]] int needs_ticks_per_second() const {
+    if (!screen_stack.empty()) {
+      return screen_stack.back()->needs_ticks_per_second();
+    }
+    return 0;
+  }
 
   /**
    * Call the tick function of the top screen in the stack.
    *
    * @param now The current time point, as a monotonic steady clock time point
    */
-  void tick(std::chrono::steady_clock::time_point now) const;
+  void tick(std::chrono::steady_clock::time_point now) const {
+    assert(controller_api != nullptr && "Controller API is null");
+    if (!screen_stack.empty()) {
+      screen_stack.back()->tick(*controller_api, now);
+    }
+  }
 };
 } // namespace ui

49feb392b4631f984fc2ae60c6a334c268b236e0
Davide Depau <davide@depau.eu>
2025-12-26 18:17:33 +0100

    feat: make ui_session screen stack render by default

--- custom-menu/ui/include/ui/ui_session.hpp
+++ custom-menu/ui/include/ui/ui_session.hpp
@@ -48,15 +48,41 @@ public:
   }
 
   /**
+   * Push a new screen onto the stack
+   *
+   * @param screen The screen to push
+   */
+  template<typename T>
+  T &push_screen_norender(std::unique_ptr<T> screen) {
+    static_assert(sdk19compat::derived_from<T, screens::iscreen> == true);
+    screen_stack.push_back(std::move(screen));
+    return *static_cast<T *>(screen_stack.back().get());
+  }
+
+  /**
    * Push a new screen onto the stack and render it.
    *
    * @param screen The screen to push
    */
   template<typename T>
   T &push_screen(std::unique_ptr<T> screen) {
+    T &result = push_screen_norender(std::move(screen));
+    render();
+    return result;
+  }
+
+  /**
+   * Replace the top screen in the stack with a new screen
+   *
+   * @param screen The screen to replace with
+   */
+  template<typename T>
+  T &replace_screen_norender(std::unique_ptr<T> screen) {
     static_assert(sdk19compat::derived_from<T, screens::iscreen> == true);
-    screen_stack.push_back(std::move(screen));
-    return *static_cast<T *>(screen_stack.back().get());
+    if (!screen_stack.empty()) {
+      screen_stack.pop_back();
+    }
+    return push_screen(std::move(screen));
   }
 
   /**
@@ -66,11 +92,19 @@ public:
    */
   template<typename T>
   T &replace_screen(std::unique_ptr<T> screen) {
-    static_assert(sdk19compat::derived_from<T, screens::iscreen> == true);
+    T &result = replace_screen_norender(std::move(screen));
+    render();
+    return result;
+  }
+
+  /**
+   * Pop the top screen from the stack.
+   */
+  void pop_screen_norender() {
+    assert(controller_api != nullptr && "Controller API is null");
     if (!screen_stack.empty()) {
       screen_stack.pop_back();
     }
-    return push_screen(std::move(screen));
   }
 
   /**
@@ -78,10 +112,8 @@ public:
    * If the stack becomes empty, return to the main menu.
    */
   void pop_screen() {
-    assert(controller_api != nullptr && "Controller API is null");
-    if (!screen_stack.empty()) {
-      screen_stack.pop_back();
-    }
+    pop_screen_norender();
+    render();
   }
 
   /**
